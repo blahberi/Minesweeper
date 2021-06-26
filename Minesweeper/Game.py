2d68d49dd30c4419b83e5a206b6afaa912daf0c8
blahberi <along.blabri@gmail.com>
2021-06-26 16:19:18 +0300

    minesweeper for console works

--- Minesweeper/Game.py
+++ Minesweeper/Game.py
@@ -5,15 +5,16 @@ class __Board:
         self.dim = dim
         self.board = [] # board has numbers which indicate how many bombs are around
         """ extra numbers:
-        10 not revealed
-        11 flag
-        12 question mark
+        10 bomb
+        11 not revealed
+        12 flag
+        13 question mark
         """
-        for i in range(self.dim[0]):
+        for i in range(self.dim[1]):
             self.board.append([])
         for row in self.board:
-            for i in range(dim[1]):
-                row.append(10)
+            for i in range(dim[0]):
+                row.append(11)
 
 
 class Game(__Board):
@@ -24,37 +25,60 @@ class Game(__Board):
         self.GAME_OVER = False
 
     def select(self, location):
-        if not self.GAME_OVER:
-            if not self.didSelect:
-                self.__create_bombs()
-                self.didSelect = False
-            if location in self.bombs:
-                print('oops you selected a bomb GAME OVER')
-                self.GAME_OVER = True
-                return
-            bomb_count = 0
-            x_list_temp = (location[0] - 1, location[0], location[0] + 1)
-            y_list_temp = (location[1] - 1, location[0], location[0] + 1)
-            x_list = []
-            y_list = []
-            for x in x_list_temp:
-                if x >= 0:
-                    x_list.append(x)
-            for y in y_list_temp:
-                if y >= 0:
-                    y_list.append(y)
+        if self.board[location[1]][location[0]] != 11:
+            return
+        if self.GAME_OVER:
+            return
+        if not self.didSelect:
+            self.__create_bombs(location)
+            self.didSelect = True
+        if location in self.bombs:
+            self.GAME_OVER = True
+        bomb_count = 0
+        x_list_temp = (location[0] - 1, location[0], location[0] + 1)
+        y_list_temp = (location[1] - 1, location[1], location[1] + 1)
+        x_list = []
+        y_list = []
+        for x in x_list_temp:
+            if x >= 0 and x < self.dim[0]:
+                x_list.append(x)
+        for y in y_list_temp:
+            if y >= 0 and y < self.dim[1]:
+                y_list.append(y)
 
+        for x in x_list:
+            for y in y_list:
+                if (x, y) in self.bombs:
+                    bomb_count += 1
+        self.board[location[1]][location[0]] = bomb_count
+        if bomb_count == 0:
             for x in x_list:
                 for y in y_list:
+                    self.select((x, y))
+        return
 
-
-    def __create_bombs(self):
+    def __create_bombs(self, location):
         # create bombs
         self.bombs = []
+        x_list_temp = (location[0] - 1, location[0], location[0] + 1)
+        y_list_temp = (location[1] - 1, location[1], location[1] + 1)
+        x_list = []
+        y_list = []
+        for x in x_list_temp:
+            if x >= 0 and x <=self.dim[0]:
+                x_list.append(x)
+        for y in y_list_temp:
+            if y >= 0 and y <= self.dim[1]:
+                y_list.append(y)
         for i in range(self.bombs_amount):
-            x = random.randint(0, self.dim[0])
-            y = random.randint(0, self.dim[1])
-            self.bombs.append((x, y))
+            while True:
+                x = random.randint(0, self.dim[0])
+                y = random.randint(0, self.dim[1])
+                if x not in x_list or y not in y_list:
+                    if (x, y) not in self.bombs:
+                        self.bombs.append((x, y))
+                        break
+        return
 
     def __str__(self):
         res = ''
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from Minesweeper.Game import Game
-minesweeper = Game((8, 8))
-print(minesweeper)
+minesweeper = Game((8, 8), 10)
+while True:
+    print(minesweeper)
+    location_x = int(input('x: '))
+    location_y = int(input('y: '))
+    minesweeper.select((location_x, location_y))
+    if minesweeper.GAME_OVER:
+        print('GAME OVER')
+        break
